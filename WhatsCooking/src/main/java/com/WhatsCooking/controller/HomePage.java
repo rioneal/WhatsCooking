619112a6df9abd8674476b92f59d25659ce587bc
stedixon <Stephanie.dixon921@gmail.com>
2017-10-23 16:08:02 -0500

    Add files via upload

---
+++ WhatsCooking/src/main/java/com/WhatsCooking/controller/HomePage.java
@@ -0,0 +1,21 @@
+package com.WhatsCooking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePage {
+
+    @RequestMapping("/")
+    public String loginPage(){ return "login_page"; }
+
+    @RequestMapping("/search")
+    public String searchPage(){ return "home"; }
+
+    @RequestMapping("/saved")
+    public String savedRecipes(){ return "saved_recipe"; }
+
+    @RequestMapping("/grocery")
+    public String groceryList(){ return "GroceryList"; }
+
+}
