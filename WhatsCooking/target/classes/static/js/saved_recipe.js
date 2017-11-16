d89de7a8720fa928c7ab12e845854da34fcab9cc
Andrew Salopek <dylan.salopek@gmail.com>
2017-11-16 14:10:21 -0600

    json stringify
    - last merge did not include stringify

--- WhatsCooking/target/classes/static/js/saved_recipe.js
+++ WhatsCooking/target/classes/static/js/saved_recipe.js
@@ -3,6 +3,8 @@ function saveRecipe(rID) {
 		if (recipeList[i].id == rID) {
 			recipe = recipeList[i];
 			console.log(recipe);
+			jsonrecipe = JSON.stringify(recipe);
+		//	Make call to DB endpoint here
 		}
 	}
 }
