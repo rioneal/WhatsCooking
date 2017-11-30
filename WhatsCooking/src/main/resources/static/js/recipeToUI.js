e7e26d5bb8d4cceb3b1d081b5f01450ff42d7316
Andrew Salopek <dylan.salopek@gmail.com>
2017-11-29 18:01:35 -0600

    Similar Recipes
    - similar recipes are shown if the user has saved recipes

--- WhatsCooking/src/main/resources/static/js/recipeToUI.js
+++ WhatsCooking/src/main/resources/static/js/recipeToUI.js
@@ -22,6 +22,7 @@ function savedMap(recipe) {
             // $("#modals").append('<div class="modal fade dynModal" id="modal' + field.id + '" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">' + field.title + '</h4></div> <div class="modal-body"><div class="row"><div class="col-lg-4"><img class="img-fluid rounded" src="' + field.image + '" HEIGHT="250" WIDTH="220" BORDER="0" style="float: left;"></div><div class="col-sm-offset-1 col-sm-6"><p><b>Needed Ingredients: </b>' + missedIngr(field.missedIngredients) + '</p><b>Your Ingredients: </b>' + usedIngr(field.usedIngredients) + '</p></div></div><p><b>Instructions</b>' + instr(field.analyzedInstructions) + '</p></div><div class="modal-footer"><button type="button" class="btn btn-primary" data-dismiss="" style="float: left;" onclick="saveRecipe(' + field.id + ')">Save</button><button type="button" class="btn btn-danger" data-dismiss="modal">Close</button></div></div></div></div>');
         }
     });
+    similar(recipe)
 }
 function groceryMap(recipe) {
     var count = 0;
--- WhatsCooking/src/main/resources/static/js/similar.js
+++ WhatsCooking/src/main/resources/static/js/similar.js
@@ -1,14 +1,18 @@
-function similar(rID) {
+function similar(recipes) {
+	var id = recipes[0].id;
 	$.ajax({
-		url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + rID + '/similar', // The URL to the API. You can get this in the API page of the API you intend to consume
+		url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + id + '/similar', // The URL to the API. You can get this in the API page of the API you intend to consume
 		type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-		data: {}, // Additional parameters here
+		// Additional parameters here
 		dataType: 'json',
 		success: function (data) {
-			similarMapper(data);
+			// console.log(data.results);
+			// console.log(JSON.stringify(data.results));
+			carousel(data)
 		},
 		error: function (err) {
 			alert(err);
+			console.log(err);
 		},
 		beforeSend: function (xhr) {
 			xhr.setRequestHeader("X-Mashape-Key", "FEVql0chRwmshPvhp3dPwwUBrnIEp1YtlVOjsnTrd2lxtjFWjW"); // Enter here your Mashape key
@@ -16,6 +20,16 @@ function similar(rID) {
 	});
 }
 
-function similarMapper(recipes){
-	
+function carousel(recipes) {
+
+	var count = 0;
+	// console.log(recipe);
+	$("#carouselSaved").append('<br/><br/><h3 style="text-align: center">Recipe Recommendations Based on Your Saved Recipes</h3>')
+	$.each(recipes, function (i, field) {
+		count++;
+		// console.log(field);
+		if (count <= 3) {
+			$("#carouselSaved").append('<div class="col-md-4"><div class="thumbnail"><a href="#"><img src="https://spoonacular.com/recipeImages/' + field.image + '" alt="' + field.title + '" style="width:100px;height:100px"><div class="caption"><p>'+field.title+'</p></div></a></div></div>')
+		}
+	});
 }
